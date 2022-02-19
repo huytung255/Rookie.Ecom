bc6812d0736495c944a48060b72f14bdf477f1a1
huytung255 <61749223+huytung255@users.noreply.github.com>
2022-02-20 00:59:00 +0700

    Add project files.

---
+++ src/Rookie.Ecom.Customer/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rookie.Ecom.Business.Interfaces;
+using System.Threading.Tasks;
+
+namespace Rookie.Ecom.Customer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index(string name,int page = 1, int limit = 10)
+        {
+            var result = await _categoryService.PagedQueryAsync(name, page, limit);
+            return View(result);
+        }
+    }
+}
