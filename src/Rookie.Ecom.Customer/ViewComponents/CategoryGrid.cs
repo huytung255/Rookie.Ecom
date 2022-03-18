4bc543180c0074677c2194013e4e98f49cdd2470
huytung255 <61749223+huytung255@users.noreply.github.com>
2022-03-18 12:34:10 +0700

    implement category grid

---
+++ src/Rookie.Ecom.Customer/ViewComponents/CategoryGrid.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rookie.Ecom.Business.Interfaces;
+using Rookie.Ecom.Contracts.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Rookie.Ecom.Customer.ViewComponents
+{
+    public class CategoryGrid : ViewComponent
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryGrid(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categoryList = await _categoryService.GetAllAsync();
+            return View(categoryList);
+        }
+    }
+}
